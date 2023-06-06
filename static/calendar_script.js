a18f09a603a12e38bcf39e684af1d5bbd233f5f0
Krzysztof S <srokr438@gmail.com>
2023-06-06 23:12:24 +0200

    added calendar, only frontend, no functionality

--- static/calendar_script.js
+++ static/calendar_script.js
@@ -1,4 +1,56 @@
-const daysTag = document.querySelector(".days"),
+const currentDate = document.querySelector(".current-date"),
+daysTag = document.querySelector(".days"),
+prevNextIcon = document.querySelectorAll(".icons span");
+
+let date = new Date(),
+currYear = date.getFullYear()
+currMonth = date.getMonth();
+
+const months = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"]
+
+const renderCalendar = () => {
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(),   //pierwszy dzień miesiąca
+    lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate(),   //ostatnia data miesiąca
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(),   //ostatni dzień miesiąca
+    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate();   //ostatnia data poprzedniego miesiąca
+
+    let liTag = "";
+
+    for (let i = firstDayofMonth; i > 0; i--){
+        liTag += `<li class="inactive">${lastDateofLastMonth - i + 1}</li>`;
+    }
+
+    for (let i = 1; i <= lastDateofMonth; i++){
+        let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
+                    && currYear === new Date().getFullYear() ? "active" : "";
+        liTag += `<li class="${isToday}">${i}</li>`;
+    }
+
+    for (let i = lastDayofMonth; i < 6; i++){
+        liTag += `<li class="inactive">${i - lastDayofMonth + 1}</li>`;
+    }
+
+    currentDate.innerText = `${months[currMonth]} ${currYear}`;
+    daysTag.innerHTML = liTag;
+}
+renderCalendar()
+
+prevNextIcon.forEach(icon => {
+    icon.addEventListener("click", () => {
+        currMonth = icon.id === "prev" ? currMonth-1 : currMonth+1;
+
+        if(currMonth < 0 || currMonth > 11){
+            date = new Date(currYear, currMonth);
+            currYear = date.getFullYear();
+            currMonth = date.getMonth();
+        } else {
+            date = new Date();
+        }
+        renderCalendar();
+    })
+});
+
+/*const daysTag = document.querySelector(".days"),
 currentDate = document.querySelector(".current-date"),
 prevNextIcon = document.querySelectorAll(".icons span");
 // getting new date, current year and month
@@ -44,4 +96,4 @@ prevNextIcon.forEach(icon => { // getting prev and next icons
         }
         renderCalendar(); // calling renderCalendar function
     });
-});
+});*/
